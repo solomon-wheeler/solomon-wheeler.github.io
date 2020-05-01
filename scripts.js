85506775efc5bcf4801c6a02bec0e89c30b19716
solomon-wheeler <63405769+solomon-wheeler@users.noreply.github.com>
2020-05-01 14:55:02 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ var Days = (new Date() - new Date(2002, 08, 09));
 Days = Math.round(Days/(86400000));
 var Years = Math.floor(Days /(365));
 document.getElementById("demo").innerHTML = "I'm currently " +  Days + " Days (" + Years + " Years ) Old"
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > screen.height || document.documentElement.scrollTop > screen.height) { //strange chrome os bug causes this not towkr work with just body.scrollTop.
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
