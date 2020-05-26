c1a539bc22e0dc1065154e080d0e48d40566ff62
solomon-wheeler <63405769+solomon-wheeler@users.noreply.github.com>
2020-05-26 19:19:20 +0100

    Reduced file sizes for files associated to index.html to make load times faster

--- scripts.js
+++ scripts.js
@@ -1,15 +1,1 @@
-//Wow! You've really dug deep to find this! Unfortunately it's exceedinly boring
-var Days = (new Date() - new Date(2002, 08, 09));
-Days = Math.round(Days/(86400000));
-var Years = Math.floor(Days /(365));
-document.getElementById("demo").innerHTML = "I'm currently " +  Days + " Days (" + Years + " Years ) Old"
-
-window.onscroll = function() {myFunction()};
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > screen.height || document.documentElement.scrollTop > screen.height) { //strange chrome os bug causes this not towkr work with just body.scrollTop.
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    }
-}
+var Days=new Date-new Date(2002,8,9);Days=Math.round(Days/864e5);var Years=Math.floor(Days/365);function myFunction(){var e=document.getElementById("myNavbar");document.body.scrollTop>screen.height||document.documentElement.scrollTop>screen.height?e.className="w3-bar w3-card w3-animate-top w3-white":e.className=e.className.replace(" w3-card w3-animate-top w3-white","")}document.getElementById("demo").innerHTML="I'm currently "+Days+" Days ("+Years+" Years ) Old",window.onscroll=function(){myFunction()};
